521819aa7b6e5dea9eaa28e51130816dea0568ff
SeraphJACK <seraphjack@outlook.com>
2017-06-10 19:20:23 +0800

    Fix some typo.
    Fix some typo.

--- src/main/java/io/github/seraphjack/enderclay/achievement/AchievementLoader.java
+++ src/main/java/io/github/seraphjack/enderclay/achievement/AchievementLoader.java
@@ -5,8 +5,9 @@ import io.github.seraphjack.enderclay.item.ItemLoader;
 import net.minecraft.stats.Achievement;
 
 public class AchievementLoader {
-    public static Achievement balance = new Achievement("achievement.enderclay.balance","enderclay.balance",-8,-6, ItemLoader.ItemEnderClay ,null);
+    public static Achievement balance;
     public AchievementLoader(){
+        balance = new Achievement("achievement.enderclay.balance","enderclay.balance",-8,-6, ItemLoader.ItemEnderClay ,null);
         balance.setSpecial();
         balance.registerStat();
     }
